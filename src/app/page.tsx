b621e82cd5ddd3ccc29e12c4c5621ed35db19ad1
Proboskis <nikolasurla22@gmail.com>
2023-03-30 00:42:49 +0200

    5. minor fixes on the homepage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
       <div className="min-h-screen min-w-screen bg-no-repeat bg-cover bg-fixed relative flex flex-col justify-start
             overflow-hidden" style={{backgroundImage: `url(${bgImage.src})`}}>
             <div className="min-h-screen flex justify-center items-center">
-                <h1 className="text-[160px] text-white font-bold tracking-wide">Staklo Design</h1>
+                <h1 className="ml-10 text-[11.25rem] text-white font-bold tracking-wide">Staklo Design</h1>
             </div>
-            <div className="min-h-screen w-2/3 top-0 left-[50%] absolute bg-gray-400 inline-block -skew-x-[26deg]
+            <div className="min-h-screen w-2/3 top-0 left-[50%] absolute bg-teal-900 bg-gray-400 inline-block -skew-x-[26.4deg]
                 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-l border-neutral-400 z-10">
                 &nbsp;
             </div>
