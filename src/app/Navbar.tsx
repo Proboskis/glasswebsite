e7b3a1d002febaeedc9bb5fc4775c401255cbbde
Proboskis <nikolasurla22@gmail.com>
2023-04-03 14:50:34 +0200

    5. refactoring and minor changes to the main page, work on the navbar.

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,11 +1,26 @@
+import Link from 'next/link'
+
+
 export default function Navigation() {
     return (
         <div className="min-w-screen flex justify-center w-auto">
-            <ul className="flex flex-row justify-around w-[80rem] border-white text-2xl font-bold text-white border-y-4 py-14">
-                <li>Početna</li>
-                <li>O nama</li>
-                <li>Ogledala</li>
-                <li>Kontakt</li>
+            <ul className="flex flex-row justify-around w-[50rem] border-white text-xl font-thin text-white border-y py-8">
+                <li>
+                    <Link href="#" className="relative after:content-[''] after:absolute after:block after:w-[100%]
+                    after:h-1 after:bg-orange-600 after:scale-x-0 after:transform-gpu after:duration-300
+                    hover:after:scale-x-[1]">
+                        početna
+                    </Link>
+                </li>
+                <li>
+                    <Link href="#">o nama</Link>
+                </li>
+                <li>
+                    <Link href="#">gledala</Link>
+                </li>
+                <li>
+                    <Link href="#">kontakt</Link>
+                </li>
             </ul>
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,14 @@ export default function Home() {
   return (
       <div className="min-h-screen min-w-screen bg-no-repeat bg-cover bg-fixed relative flex flex-col justify-center
             overflow-hidden" style={{backgroundImage: `url(${bgImage.src})`}}>
-            <div className="h-full w-screen top-0 left-0 absolute bg-teal-900 inline-block
-                bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 flex flex-col justify-around">
+            <div className="h-screen w-screen top-0 left-0 absolute bg-teal-900 inline-block
+                bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 flex flex-col justify-around p-24">
                 <Navbar />
-                <div className="flex justify-center items-center">
-                    <h1 className="text-9xl text-white font-bold tracking-wide mb-36 z-10">Staklo Design</h1>
+                <div className="flex justify-center">
+                    <h1 className="text-9xl text-white font-thin tracking-wide">Staklo Design</h1>
+                </div>
+                <div className="flex-justify-center">
+                    <p className="text-white text-md font-thin text-center">vam nudi ogledala po meri, termopan staklo, kaljeno staklo za kuhinje, kao i obradu, bušenje, brušenje i peskiranje stakla.</p>
                 </div>
             </div>
       </div>
