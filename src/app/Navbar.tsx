1c8f86eedb1dca915b04c3996999ea4e52b12547
Proboskis <nikolasurla22@gmail.com>
2023-04-04 14:39:15 +0200

    6. Fixed navigation hover animation.

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -6,9 +6,9 @@ export default function Navigation() {
         <div className="min-w-screen flex justify-center w-auto">
             <ul className="flex flex-row justify-around w-[50rem] border-white text-xl font-thin text-white border-y py-8">
                 <li>
-                    <Link href="#" className="relative after:content-[''] after:absolute after:block after:w-[100%]
-                    after:h-1 after:bg-orange-600 after:scale-x-0 after:transform-gpu after:duration-300
-                    hover:after:scale-x-[1]">
+                    <Link href="#" className="relative px-3 after:content-[''] after:absolute after:block after:w-[100%]
+                    after:h-[3px] after:bg-orange-600 after:scale-x-0 after:transform-gpu after:duration-300 after:my-5
+                    after:rounded-md hover:after:scale-x-[1]">
                         početna
                     </Link>
                 </li>
